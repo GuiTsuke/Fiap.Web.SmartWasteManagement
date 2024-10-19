a4ac5109e209fcd235eeb4d784efc30f31d305b1
GuiTsuke <guilhermebborges04m2@gmail.com>
2024-10-18 21:28:56 -0300

    Inicializando meu projeto

--- Fiap.Web.SmartWasteManagement.Tests/obj/Debug/net8.0/Fiap.Web.SmartWasteManagement.Tests.AssemblyInfo.cs
+++ Fiap.Web.SmartWasteManagement.Tests/obj/Debug/net8.0/Fiap.Web.SmartWasteManagement.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fiap.Web.SmartWasteManagement.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e3dfc6fbe8145f4124eda1265c843379df5df7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08f65d4c24539868905dd1c292fbeaec3e2e158")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fiap.Web.SmartWasteManagement.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fiap.Web.SmartWasteManagement.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
